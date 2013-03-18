d121296c75d28a0e26f20095f75e9adffbee1554
Pratyush Nigam <pratyush.nigam@gmail.com>
2013-03-18 08:45:46 +0530

    Update combinationSum.cpp

--- leetcode/combinationSum.cpp
+++ leetcode/combinationSum.cpp
@@ -1,30 +1,38 @@
 class Solution {
-	public:
-		vector<vector<int> >dp(vector<int> c, int t, vector<int> v, vector<vector<int> > p, int sum,int k)
-		{
-			if(sum == t)
-			{
-				p.push_back(v);
-				return p;
-			}
-			else if(sum > t)
-				return p;
-			//vector<vector<int> > y;
-			int i;
-			for(i= k;i<c.size();i++)
-			{
-				v.push_back(c[i]); 
-				p=dp(c,t,v,p,sum+c[i],i);
-				v.pop_back();
-			}
-			return p;
-		}
-		vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
-			// Start typing your C/C++ solution below
-			// DO NOT write int main() function
-			sort(candidates.begin(),candidates.end());
-			vector<int> v;
-			vector<vector<int> > p;
-			return dp(candidates,target,v,p,0,0);
-		}
+public:
+    void f(int i, vector<vector<int> > &v,vector<int> a,int sum,int t,vector<int> c)
+    {
+        if(i==c.size())
+        {
+            if(sum == t)
+                v.push_back(a);
+            return;
+        }
+        if(sum == t)
+        {
+            v.push_back(a);
+            return;
+        }
+        if(sum > t)
+            return;
+        
+        int s = sum;
+        int j=0;
+        while(s+(j*c[i])<=t)
+        {
+            f(i+1,v,a,s+(j*c[i]),t,c);
+            a.push_back(c[i]);
+            j++;
+        }
+        return;
+    }
+    vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        vector<int> a;
+        vector<vector<int> > v;
+        sort(candidates.begin(),candidates.end());
+        f(0,v,a,0,target,candidates);
+        return v;
+    }
 };
