38808d5c5b4ca8f2b2c40ed18f00be75efd79026
Pratyush Nigam <pratyush.nigam@gmail.com>
2013-03-18 08:39:28 +0530

    Update combinationSum2.cpp

--- leetcode/combinationSum2.cpp
+++ leetcode/combinationSum2.cpp
@@ -1,48 +1,38 @@
 class Solution {
-	public:
-		vector<vector<int> >dp(vector<int> c, int t, vector<int> v, vector<vector<int> > p, int sum,int k)
-		{
-			if(sum == t)
-			{
-				int j,k;
-				for(j=0;j<p.size();j++)
-				{
-					int flg =1;
-					if(v.size() == p[j].size())
-						for(k=0;k<p[j].size() && k<v.size();k++)
-						{
-							if(v[k]!=p[j][k])
-							{
-								flg = 0;
-								break;
-							}
-						}
-					else
-						flg = 0;
-					if(flg == 1)
-						return p;
-				}
-				p.push_back(v);
-				return p;
-			}
-			else if(sum > t)
-				return p;
-			//vector<vector<int> > y;
-			int i;
-			for(i= k;i<c.size();i++)
-			{
-				v.push_back(c[i]); 
-				p=dp(c,t,v,p,sum+c[i],i+1);
-				v.pop_back();
-			}
-			return p;
-		}
-		vector<vector<int> > combinationSum2(vector<int> &candidates, int target) {
-			// Start typing your C/C++ solution below
-			// DO NOT write int main() function
-			sort(candidates.begin(),candidates.end());
-			vector<int> v;
-			vector<vector<int> > p;
-			return dp(candidates,target,v,p,0,0);
-		}
+public:
+    void f(int i, vector<vector<int> > &v,vector<int> a,int sum,int t,vector<int> c)
+    {
+        if(i==c.size())
+        {
+            if(sum == t)
+                v.push_back(a);
+            return;
+        }
+        if(sum == t)
+        {
+            v.push_back(a);
+            return;
+        }
+        if(sum > t)
+            return;
+        
+        int s = sum;
+        int j=0;
+        while(s+(j*c[i])<=t)
+        {
+            f(i+1,v,a,s+(j*c[i]),t,c);
+            a.push_back(c[i]);
+            j++;
+        }
+        return;
+    }
+    vector<vector<int> > combinationSum(vector<int> &candidates, int target) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        vector<int> a;
+        vector<vector<int> > v;
+        sort(candidates.begin(),candidates.end());
+        f(0,v,a,0,target,candidates);
+        return v;
+    }
 };
