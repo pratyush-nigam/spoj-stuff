5ef4fb11ba7d5426d6149c309f0cf31fa070ee85
Pratyush Nigam <pratyush.nigam@gmail.com>
2013-04-07 11:54:05 +0530

    Update Implementstrstr.cpp

--- leetcode/Implementstrstr.cpp
+++ leetcode/Implementstrstr.cpp
@@ -1,34 +1,62 @@
 class Solution {
 public:
+    vector<int> clps(char *needle, int n)
+    {
+        int i = 1,len = 0;
+        vector<int> l;
+        l.push_back(0);
+        while(i<n)
+        {
+            if(needle[i]==needle[len])
+            {
+                len++;
+                l.push_back(len);
+                i++;
+            }
+            else
+            {
+                if(len==0)
+                {
+                    l.push_back(0);
+                    i++;
+                }
+                else
+                {
+                    len = l[len-1];
+                    i++;
+                }
+            }
+        }
+        return l;
+    }
     char *strStr(char *haystack, char *needle) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
-        if(*needle=='\0')
+        int m = strlen(needle);
+        int n = strlen(haystack);
+        if(m>n)
+            return NULL;
+        if(n==0)
             return haystack;
-        char *i,*j;
-        i = haystack;
-        j = needle;
-        while(*i != '\0')
+        
+        vector<int> l = clps(needle,m);
+        int i=0,j=0;
+        while(i<n)
         {
-            if(*i==*j)
+            if(needle[j] == haystack[i])
             {
-                char *t,*k;
-                t = i;
-                k = j;
-                while(*k!='\0' && *t!='\0' && *t==*k)
-                {
-                    t++;
-                    k++;
-                }
-                if(*k=='\0')
-                    return i;
-                else if(*t=='\0')
-                    return NULL;
+                j++;
+                i++;
+            }
+            if(j==m)
+                return haystack+(i-j);
+            else if(haystack[i]!=needle[j])
+            {
+                if(j!=0)
+                    j = l[j-1];
                 else
                     i++;
             }
-            else
-                i++;
         }
         return NULL;
     }
